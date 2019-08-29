addcf31edab91a542a93c9ee4126f7662e149f7e
dudekdawid89 <dudekdawid89@gmail.com>
2019-08-28 20:22:33 -0500

    server set up and basic data

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+module.exports =[
+    {
+    name: "Firend1",
+    photo:"",
+    scores: [
+        2,4,5,3,5,7,6,4,2,6
+    ]},
+    {
+    name: "",
+    photo: "",
+    scores: [
+        5,3,1,4,6,4,2,5,4,7
+    ]},
+    {
+    name: "",
+    photo: "",
+    scores: [
+        4,2,5,4,7,1,1,3,9,9
+    ]},
+    {
+    name: "",
+    photo: "",
+    scores: [
+        9,9,9,9,9,3,6,4,7,5
+    ]},
+    {
+        name: "",
+    photo: "",
+    scores: [
+        1,1,1,1,1,1,1,1,1,1
+    ]}
+]
